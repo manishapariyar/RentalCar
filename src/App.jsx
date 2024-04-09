f0c5cc2cc9d9142d64320c161128ac436b7f3685
manishapariyar <manishapariyar196@gmail.com>
2024-04-09 11:57:41 +0545

    inital commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='text-red-400'>
+      hello
+    </div>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: "class",
+  theme: {
+    extend: {
+      colors: {
+        primary: "#272eff",
+        secondary: {
+          100: "#E2E2D5",
+          200: "#888883",
+        },
+        dark: "#111111",
+      },
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: "1rem",
+          sm: "3rem",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
